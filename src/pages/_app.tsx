8bbab9e3208a8be372f2fbefa5b1152669f603c5
Pedro <pedropaivadev@gmail.com>
2023-04-30 12:59:25 -0300

    environment preparation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import {useRouter} from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  console.log(router)
+
+  return (
+      <Component {...pageProps} />
+  )
 }
