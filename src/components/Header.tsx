c53bf79c8bd477fed032450c5848ba69e3b90802
Pedro <pedropaivadev@gmail.com>
2023-05-10 11:02:46 -0300

    fix: return to NextJs version 13.3.2 and fix Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,35 +4,44 @@ import styled from "styled-components";
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-const Nav = styled.nav`
+const HeaderTag = styled.nav`
   position: fixed;
+  top: 0px;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
+  width: var(--mainWidth);
+  height: var(--HeaderHeigth);
   background-color: ${props => props.theme.colors.primaryColor};
-  width: 100%;
-  height: 60px;
-  border-bottom: 3px solid ${props => props.theme.colors.dark};
   border-top: 3px solid ${props => props.theme.colors.dark};
-  a {
-    border-radius: 20px;
-    transition: 0.3s;
-    text-shadow: 1px 1px 5px ${props => props.theme.colors.dark};
-    line-height: 26px;
-    text-align: center;
-    vertical-align: middle;
-    &:hover, &.active {
-      background: ${props => props.theme.colors.tertiaryColor};
-      box-shadow: 0 0 10px 10px ${props => props.theme.colors.tertiaryColor};
-      color: ${props => props.theme.colors.primaryColor};
-      text-shadow: none;
+  border-bottom: 3px solid ${props => props.theme.colors.dark};
+  z-index: 1;
+  nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    max-width: 600px;
+    a {
+      border-radius: 20px;
+      transition: 0.3s;
+      text-shadow: 1px 1px 5px ${props => props.theme.colors.dark};
+      line-height: 26px;
+      text-align: center;
+      vertical-align: middle;
+      &:hover, &.active {
+        background: ${props => props.theme.colors.tertiaryColor};
+        box-shadow: 0 0 10px 10px ${props => props.theme.colors.tertiaryColor};
+        color: ${props => props.theme.colors.primaryColor};
+        text-shadow: none;
+      }
     }
-  }
-  .logoImg {
-    border-radius: 20px;
-    transition: 0.3s;
-    cursor: pointer;    
-    &:hover {
+    .logoImg {
+      border-radius: 20px;
+      transition: 0.3s;
+      cursor: pointer;
+    }
+    .logoImg:hover, .active {
       background: ${props => props.theme.colors.tertiaryColor};
       box-shadow: 0 0 10px 10px ${props => props.theme.colors.tertiaryColor};
     }
@@ -41,24 +50,22 @@ const Nav = styled.nav`
 
 const Header = () => {
 
-  const {pathname} = useRouter();
+  const {pathname, push} = useRouter();
 
   return (
-    <header style={{width: '100%'}}>
-      <Nav>
-        <Link href='/Menu' className={pathname==='/Menu' ? 'active' : ''}>Cardápio</Link>
-        <Link href='/' className={pathname==='/' ? 'active' : ''}>
-          <img
-              src="/logo.png"
-              alt="LogoPP"
-              width={100}
-              height={50}
-              className='logoImg'
-          />
+    <HeaderTag style={{width: '100%'}}>
+      <nav>
+        <Link href='/Menu' className={pathname==='/Menu' ? 'active' : ''}>
+          Cardápio
         </Link>
-        <Link href='/Entrega' className={pathname==='/Entrega' ? 'active' : ''}>Entrega</Link>
-      </Nav>
-    </header>
+        <div className={`logoImg ${pathname==='/' ? 'active' : ''}`} onClick={() => push('/')}>
+          <img src="/logo.png" alt="LogoPP" height='50px'/>
+        </div>
+        <Link href='/Entrega' className={pathname==='/Entrega' ? 'active' : ''}>
+          Entrega
+        </Link>
+      </nav>
+    </HeaderTag>
   )
 }
 
--- src/pages/Entrega.tsx
+++ src/pages/Entrega.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Entrega = () => {
   return (
-    <div className='page'>Entrega</div>
+    <div className='page animeLeft'>
+      <div className='container'>
+        <div className='envelope'>
+          <h1>Entrega</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
