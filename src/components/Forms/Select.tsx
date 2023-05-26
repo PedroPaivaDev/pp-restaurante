1b4862f33417c8f81420ebf98fd4a12d92cb2c32
Pedro <pedropaivadev@gmail.com>
2023-05-26 18:46:44 -0300

    feat: Select component, for the user to choose the payment method;

---
+++ src/components/Forms/Select.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SelectContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 10px;
+  .select {
+    color: ${props => props.theme.colors.primaryColor};
+    border: 1px solid ${props => props.theme.colors.secondaryColor};
+    line-height: 26px;
+    padding: 2px 5px;
+    border-radius: 5px;
+    background: ${props => props.theme.colors.quintenaryColor};
+    transition: 0.2s;
+    width: 180px;
+    cursor: pointer;
+    option {
+      color: ${props => props.theme.colors.primaryColor};
+    }
+  }
+  .select:focus, .select:hover {
+    outline: none;
+    border-color: ${props => props.theme.colors.primaryColor};
+    background: ${props => props.theme.colors.tertiaryColor};
+    box-shadow: 0 0 0 3px ${props => props.theme.colors.secondaryColor}
+  }
+`;
+
+interface PropsSelect {
+  initial: string;
+  options: string[];
+  selectedOption: string;
+  setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
+  label: string;
+  className?: string;
+  value?: string;
+}
+
+const Select = ({initial, options, selectedOption, setSelectedOption, className, label, value}:PropsSelect) => {
+
+  function handleChange({target}:{target:HTMLSelectElement}) {
+    setSelectedOption(target.value)
+  }
+
+  return (
+    <SelectContainer className={className}>
+      <label htmlFor={label}>{label}</label>
+      <select id={label} value={selectedOption} onChange={handleChange} className='select'>
+        <option value="" disabled>{initial}</option>
+        {options.map((option, index) => (
+          value ?
+          <option key={option} value={value[index]}>{option}</option> :
+          <option key={option} value={option}>{option}</option>
+        ))}
+      </select>
+    </SelectContainer>
+  )
+}
+
+export default Select;
--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,21 +1,42 @@
-import useForm from '@/hooks/useForm';
 import React from 'react';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import useForm from '@/hooks/useForm';
+
 import InputText from './Forms/Input';
+import Select from './Forms/Select';
+import styled from 'styled-components';
+
+const OrderContainer = styled.div`
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+`;
 
 const Order = () => {
   const client = useForm('client', '');
-  React.useEffect(() => {
-    console.log(client.value)
-  })
+  const contact = useForm('contact', '', 'contact');
+  const [payment, setPayment] = useLocalStorage('payment', '');
+
   return (
-    <div>
+    <OrderContainer>
       <form>
         <InputText
           label="Nome:" type="text" name="client"
           placeholder={"Digite seu nome"} {...client}
         />
+        <InputText label="Contato:" type="text" name="contact"
+          placeholder={"(37) 9 9999-9999"} {...contact}
+        />
+        <Select
+          label={"Pagamento:"}
+          initial="Escolha a forma"
+          options={["Transferência", "Pix", "Cartão de Débito", "Dinheiro", "Cartão de Crédito (parcelado)"]}
+          selectedOption={payment} setSelectedOption={setPayment}
+        />
       </form>
-    </div>
+    </OrderContainer>
   )
 }
 
