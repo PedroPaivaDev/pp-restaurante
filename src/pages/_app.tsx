6579a8f8174678d1e2f8eab25706fda6a579e226
Pedro <pedropaivadev@gmail.com>
2023-05-29 12:08:12 -0300

    fix: the file 'src/pages/_document.tsx', was transformed into a class, with the static method 'getInitialProps', which is called by Next.js before rendering the page. It receives the 'DocumentContext' as an argument and creates a new 'ServerStyleSheet', to collect the application's styles. The context's 'renderPage' method is replaced to wrap the 'App' component with the 'ServerStyleSheet' using the 'collectStyles' function. This ensures that the styles are collected correctly.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head';
 
 // import '@/styles/globals.css';
 import {ThemeProvider} from 'styled-components';
-import theme from '../styles/theme';
 import GlobalStyles from '@/styles/global';
+import theme from '../styles/theme';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,51 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// https://styled-components.com/docs/advanced#server-side-rendering
+// https://github.com/vercel/next.js/blob/canary/examples/with-styled-components-babel/pages/_document.tsx
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html lang="pt-BR">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='use-credentials' />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />        
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  //Adicionar contexto do proprio next chamado (DocumentContext)
+  //Que serve pra poder manipular o TSX para validação
+
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='use-credentials' />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />        
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
